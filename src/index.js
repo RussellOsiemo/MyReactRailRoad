5bdd7532f25e19b3b95ffe05e77c170625586a86
Russel Osiemo <86182360+RussellOsiemo@users.noreply.github.com>
2021-10-21 12:22:27 +0300

    using map to get elements in array

--- src/index.js
+++ src/index.js
@@ -5,40 +5,43 @@ import ReactDom from 'react-dom';
 //capitalize the first letter of the name of the function
 //css 
 import './index.css';
-const firstbook = {
-   Image :"https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL160_SR160,160_.jpg ",
-  Title : 'DUNE : AUDIOBOOK',
-  Author:'Frank Herbert'
+const books = [
+{
+    Image: "https://images-na.ssl-images-amazon.com/images/I/91JV0+3BRGL._AC_UL160_SR160,160_.jpg ",
+    Title: 'DUNE : AUDIOBOOK',
+    Author: 'Frank Herbert'
+},
+{
+    Image: "https://images-na.ssl-images-amazon.com/images/I/81PNeyIYVfL._AC_UL160_SR160,160_.jpg",
+    Title: 'Greenlights',
+    Author: 'Matthew McConaughey'
 }
-const secondbook = {
-   Image :"https://images-na.ssl-images-amazon.com/images/I/81PNeyIYVfL._AC_UL160_SR160,160_.jpg",
-  Title : 'Greenlights',
-  Author:'Matthew McConaughey'
+];
+function BookList() {
+    return ( < section className = "booklist" >
+        {/* to access objects in array books i need map  */}
+        {books.map((book) => {
+            return 'Hello';
+        })}
+        </section>
+    );
 }
+//option 3 for properties(my choice)
+const Book = ({ Image, Title, Author, children }) => {
+    //option 1 cons        ole.log(props)
+    //option 2 const {Image,Title,Author} = props
+    return <article className = 'book' >
+        <img src = { Image }
+    alt = "" />
+        <h1> { Title } </h1> 
+        <h4 > { Author } </h4>
 
-function BookList(){
-return( <section className="booklist">
-   <Book Image ={firstbook.Image} Title = {firstbook.Title} Author ={firstbook.Author} />
-   <Book Image ={secondbook.Image} Title = {secondbook.Title} Author ={secondbook.Author} />
-   <Book />
-   <Book />
-   
-</section>
-);
-}
-const Book = (props) =>{
-   console.log(props)
-  return <article className='book'>
-     <img src={props.Image} alt="" />
-   <h1>{props.Title}</h1>
-   <h4>{props.Author}</h4>
-   <p>{props.job}</p>
-   
-   </article>
-  
+
+    </article>
+
 };
 // const Image = () => 
 // const Title = () => 
 // const Author = () => 
 //renders a function Greeting to div.root on index.html
-ReactDom.render(<BookList />,document.getElementById('root'));
+ReactDom.render( < BookList / > , document.getElementById('root'));
